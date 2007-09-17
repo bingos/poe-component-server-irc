b3a1e40e5677396121dbc623ca5df620f352de3a
Chris Williams <chris@bingosnet.co.uk>
2007-09-17 15:58:55 +0000

    Removed executable property from Makefile.PL

--- lib/POE/Component/Server/IRC.pm
+++ lib/POE/Component/Server/IRC.pm
@@ -15,7 +15,7 @@ use POE::Component::Server::IRC::Plugin qw(:ALL);
 use Date::Format;
 use vars qw($VERSION $REVISION);
 
-$VERSION = '1.18';
+$VERSION = '1.20';
 ($REVISION) = (q$LastChangedRevision$=~/(\d+)/g);
 
 sub spawn {
