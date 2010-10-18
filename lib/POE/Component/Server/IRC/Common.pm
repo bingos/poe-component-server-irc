e992c087344293445503ea7fd710c3df83ce654a
Hinrik Örn Sigurðsson <hinrik.sig@gmail.com>
2010-10-18 20:54:38 +0000

    Pod cleanup of Common.pm

--- lib/POE/Component/Server/IRC/Common.pm
+++ lib/POE/Component/Server/IRC/Common.pm
@@ -277,7 +277,10 @@ POE::Component::Server::IRC::Common - provides a set of common functions for the
 
 =head1 DESCRIPTION
 
-POE::Component::IRC::Common provides a set of common functions for the L<POE::Component::Server::IRC> suite. There are included functions for uppercase and lowercase nicknames/channelnames and for parsing mode lines and ban masks.
+POE::Component::IRC::Common provides a set of common functions for the
+L<POE::Component::Server::IRC|POE::Component::Server::IRC> suite. There are
+included functions for uppercase and lowercase nicknames/channelnames and for
+parsing mode lines and ban masks.
 
 =head1 FUNCTIONS
 
@@ -285,15 +288,23 @@ POE::Component::IRC::Common provides a set of common functions for the L<POE::Co
 
 =item C<u_irc>
 
-Takes one mandatory parameter, a string to convert to IRC uppercase, and one optional parameter, the casemapping of the ircd ( which can be 'rfc1459', 'strict-rfc1459' or 'ascii'. Default is 'rfc1459' ). Returns the IRC uppercase equivalent of the passed string.
+Takes one mandatory parameter, a string to convert to IRC uppercase, and one
+optional parameter, the casemapping of the ircd (which can be 'rfc1459',
+'strict-rfc1459' or 'ascii'. Default is 'rfc1459'). Returns the IRC uppercase
+equivalent of the passed string.
 
 =item C<l_irc>
 
-Takes one mandatory parameter, a string to convert to IRC lowercase, and one optional parameter, the casemapping of the ircd ( which can be 'rfc1459', 'strict-rfc1459' or 'ascii'. Default is 'rfc1459' ). Returns the IRC lowercase equivalent of the passed string.
+Takes one mandatory parameter, a string to convert to IRC lowercase, and one
+optional parameter, the casemapping of the ircd (which can be 'rfc1459',
+'strict-rfc1459' or 'ascii'. Default is 'rfc1459'). Returns the IRC lowercase
+equivalent of the passed string.
 
 =item C<parse_mode_line>
 
-Takes a list representing an IRC mode line. Returns a hashref. If the modeline couldn't be parsed the hashref will be empty. On success the following keys will be available in the hashref:
+Takes a list representing an IRC mode line. Returns a hashref. If the modeline
+couldn't be parsed the hashref will be empty. On success the following keys
+will be available in the hashref:
 
    'modes', an arrayref of normalised modes;
    'args', an arrayref of applicable arguments to the modes;
@@ -310,7 +321,8 @@ Example:
 
 =item C<parse_ban_mask>
 
-Takes one parameter, a string representing an IRC ban mask. Returns a normalised full banmask.
+Takes one parameter, a string representing an IRC ban mask. Returns a
+normalised full banmask.
 
 Example:
 
@@ -320,43 +332,60 @@ Example:
 
 =item C<matches_mask>
 
-Takes two parameters, a string representing an IRC mask ( it'll be processed with parse_ban_mask() to ensure that it is normalised ) and something to match against the IRC mask, such as a nick!user@hostname string. Returns 1 if they match, 0 otherwise. Returns undef if parameters are missing. Optionally, one may pass the casemapping ( see u_irc() ), as this function ises u_irc() internally.
+Takes two parameters, a string representing an IRC mask (it'll be processed
+with C<parse_ban_mask()> to ensure that it is normalised) and something to
+match against the IRC mask, such as a nick!user@hostname string. Returns 1 if
+they match, undef/empty list otherwise. Returns undef if parameters are
+missing. Optionally, one may pass the casemapping (see C<u_irc()>), as this
+function uses C<u_irc()> internally.
 
 =item C<matches_mask_array>
 
-Takes two array references, the first being a list of strings representing IRC mask, the second a list of somethings to test against the masks. Returns an empty hashref if there are no matches. Matches are returned are arrayrefs keyed on the mask that they matched.
+Takes two array references, the first being a list of strings representing
+IRC mask, the second a list of somethings to test against the masks. Returns
+an empty hashref if there are no matches. Matches are returned are arrayrefs
+keyed on the mask that they matched.
 
 =item C<gen_mode_change>
 
-Takes two arguments, being a strings representing a set of IRC user modes before and after a change. Returns a string representing what changed.
+Takes two arguments, being a strings representing a set of IRC user modes
+before and after a change. Returns a string representing what changed.
 
   my $mode_change = gen_mode_change( 'abcde', 'befmZ' );
   $mode_change is now '-acd+fmZ'
 
 =item C<unparse_mode_line>
 
-Takes one argument a string representing a number of mode changes. Returns a condensed version of the changes.
+Takes one argument a string representing a number of mode changes. Returns a
+condensed version of the changes.
 
   my $mode_line = unparse_mode_line('+o+o+o-v+v');
   $mode_line is now '+ooo-v+v'
 
 =item C<validate_chan_name>
 
-Takes one argument a channel name to validate. Returns true or false if the channel name is valid or not.
+Takes one argument a channel name to validate. Returns true or false if the
+channel name is valid or not.
 
 =item C<validate_nick_name>
 
-Takes one argument a nickname to validate. Returns true or false if the nickname is valid or not.
+Takes one argument a nickname to validate. Returns true or false if the
+nickname is valid or not.
 
 =item C<parse_user>
 
-Takes one parameter, a string representing a user in the form nick!user@hostname. In a scalar context it returns just the nickname. In a list context it returns a list consisting of the nick, user and hostname, respectively.
+Takes one parameter, a string representing a user in the form
+nick!user@hostname. In a scalar context it returns just the nickname. In a
+list context it returns a list consisting of the nick, user and hostname,
+respectively.
 
 =item C<mkpasswd>
 
-Takes one mandatory argument a plain string to 'encrypt'. If no further options are specified it uses C<crypt> to generate the
-password. Specifying 'md5' option uses L<Crypt::PasswdMD5>'s C<unix_md5_crypt> function to generate the password. Specifying 
-'apache' uses L<Crypt::PasswdMD5>'s C<apache_md5_crypt> function to generate the password.
+Takes one mandatory argument a plain string to 'encrypt'. If no further
+options are specified it uses C<crypt> to generate the password. Specifying
+'md5' option uses L<Crypt::PasswdMD5>'s C<unix_md5_crypt> function to
+generate the password. Specifying  'apache' uses L<Crypt::PasswdMD5>'s
+C<apache_md5_crypt> function to generate the password.
 
   my $passwd = mkpasswd( 'moocow' ); # vanilla crypt()
 
@@ -366,8 +395,9 @@ password. Specifying 'md5' option uses L<Crypt::PasswdMD5>'s C<unix_md5_crypt> f
 
 =item C<chkpasswd>
 
-Takes two mandatory arguments, a password string and something to check that password against. The function first tries md5 
-comparisons ( UNIX and Apache ), then C<crypt> and finally plain-text password check.
+Takes two mandatory arguments, a password string and something to check that
+password against. The function first tries md5 comparisons (UNIX and Apache),
+then C<crypt> and finally plain-text password check.
 
 =back
 
@@ -379,8 +409,12 @@ Chris 'BinGOs' Williams
 
 Copyright E<copy> Chris Williams
 
-This module may be used, modified, and distributed under the same terms as Perl itself. Please see the license that came with your Perl distribution for details.
+This module may be used, modified, and distributed under the same terms as
+Perl itself. Please see the license that came with your Perl distribution
+for details.
 
 =head1 SEE ALSO
 
 L<POE::Component::Server::IRC>
+
+=cut
